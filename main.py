f0e32f0117805cea2396509921c6cb35fd2e5d85
Aravind Pedapudi <aravindp1510@gmail.com>
2013-01-27 10:57:38 +0530

    Encoding and Decoding a long string of numbers

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 import array
 import math
 import itertools
-num="152487216054048971758341586351234728397473961424346"
+#num="152487216054048971758341586351234728397473961424346"
+num="152472160540471753415635123472374736142434611564879462125641652562165487921564987923131636662156898716654987213223165479879542136464989878921321611321111111165496498469612132165654231363231645648931632156"
 mod = 4 #divide 0-9 in groups of mod
 maxinum = 9 
 f1=open('plaintext','w')
@@ -21,8 +22,8 @@ for x in num:
     output[1] += '1'*line+'0'
     binterm=bin(int(x)).split('b')[1]
     output[line+1] += '0'*(n-len(binterm))+binterm[-n:]
-
-for i in range(0,len(output)):
+output[0]=str(mod)+' ';
+for i in range(1,len(output)):
     line=output[i]
     if line!='':
         output[i] = output[i] +  ( '0'*(8-len(line)%8) if (len(line)%8) else '')
@@ -40,3 +41,39 @@ for i in range(1,len(output)):
         print x, int(''.join(x),2)
         f2.write(chr(int(''.join(x),2)))
 f2.close()
+
+#Decoding
+f3 = open('binary','rb')
+#first line tells us the number of characters on each level
+fline=f3.readline()
+#each value is separated by a space, split by spaces
+numChars=fline.split(' ')
+#form input array:
+finput=[]
+for x in numChars[1:]:
+    finput.append(f3.read(int(x)))
+#the first value in fline will give us the input mod
+f3.close()
+mod = int(numChars[0])
+n = int(math.log(mod,2))
+#we have the input from the file, we now need to decode it
+#first line describes the position of each number. Each position is written in the form of ones. Each position is separated by a '0' So, 101110111101011 will give [1,3,4,1,2]
+#output = ''
+#build binary from ascii
+binaryLines=['']*len(finput)
+for line in range(0,len(finput)):
+    binline=''
+    for x in finput[line]:
+        binterm=bin(ord(x)).split('b')[1]
+        binline+= '0'*(8-len(binterm)) + binterm
+    binaryLines[line]=binline
+    
+positions = filter(bool,binaryLines[0].split('0'))
+outputnum=''
+for pos in positions:
+    line = pos.count('1')
+    outputnum+=str(int(bin(line-1).split('b')[1] + binaryLines[line][0:n],2))
+    binaryLines[line]=binaryLines[line][n:]
+f4 = open('decodedPlainText','w')
+f4.write(outputnum)
+f4.close()
